1905f49e10534aa9a9da3421d1e83a572a6ab15b
roeis <roeichow@gmail.com>
2015-09-01 14:21:59 +0800

    update

---
+++ js/mu/mu.slider.js
@@ -0,0 +1,277 @@
+/**
+ * -------------------------------------------------------------
+ * Copyright (c) 2015 All rights reserved.
+ * @version: 1.1.0
+ * @author: roeis
+ * @description: extend slider from swipeable
+ * @dependency: $.fn.swipeable
+ * -------------------------------------------------------------
+ */
+
+(function($) {
+    'use strict';
+
+    var Slider = function(el, opts) {
+        this.$el = $(el);
+        this.opts = opts;
+        this.init();
+    };
+    var defaults = {
+        autoSlide: false,
+        speed: 500,
+        isLoop: false,
+        isVert: false,
+        beforeSlide: function() {},
+        afterSlide: function() {}
+    };
+    Slider.prototype = {
+        init: function() {
+            this.opts = $.extend({}, defaults, this.opts);
+            this._create();
+            this._bind();
+        },
+        _create: function() {
+            var self = this;
+            self.$children = self.$el.children();
+            self.max = self.$children.length;
+            self.animating = false;     //正在动画
+            self.isMoving = false;      //正在手势滑动
+            self.looptime = null;
+            self.index = 0;             // 起始序号
+            self.clones = 0;            // 克隆数
+
+            if(self.opts.isLoop){
+                self.index = 1;
+                self.clones = 2;
+                self.max += self.clones;
+            }
+
+            self.$children.wrapAll('<div class="slider-wrap"></div>');
+            // maintain each instance standlone, when initialize lot of instance with a same class
+            // in jump function, the active target is self.$slider, and this would be current target's parent
+            self.$slider = self.$children.closest('.slider-wrap');
+
+            self.$el.css({
+                'overflow': 'hidden',
+                'position': 'relative'
+            });
+
+            self.$slider.css({'position' : 'absolute'});
+
+            if(self.opts.isVert){
+                //ATTENTION: prevent global touchmove event
+                window.mu && mu.util.preventScroll();
+                self.$slider.css({'height': self.max * 100 + '%', 'width': '100%'});
+                self.$children.css({'height': 100 / self.max + '%'});
+            }else{
+                self.$slider.css({'width': self.max * 100 + '%'});
+                self.$children.css({'width': 100 / self.max + '%', 'height': '100%','float': 'left'});
+            }
+
+            if(self.opts.isLoop){
+                self._setClone();
+            }
+
+            self._jump(self.index);
+
+            if(self.opts.autoSlide){
+                self.loop();
+            }
+        },
+        _setClone: function(){
+            if(this.opts.isLoop){
+                var $cloneFirst = this.$children.eq(0).clone().addClass('mu-clone'),
+                    $cloneLast = this.$children.eq(this.max - this.clones - 1).clone().addClass('mu-clone');
+
+                this.$slider.prepend($cloneLast).append($cloneFirst);
+            }
+        },
+        loop: function(){
+            var self = this;
+            if(self.index >= self.max - 1 ){
+                // self.stopLoop();
+                self.index = this.clones - 1;
+                // return;
+            }
+            
+            self.looptime = setTimeout(function(){
+                // Plus index before transform, attention when loop
+                self.index ++;
+                self.jump(self.index);
+                self.loop();
+            }, 3000);
+            
+        },
+        stopLoop: function(){
+            clearTimeout(this.looptime);
+        },
+        _bind: function() {
+            var startPoint = 0,
+                self = this,
+                parentTop = self.$el.offset().top,
+                parentLeft = self.$el.offset().left;
+
+            self.$el.swipeable({
+                enableVertical: self.opts.isVert,
+                start: function(data) {
+                    if(self.opts.autoSlide){
+                        self.stopLoop();
+                    }
+                    // fix slider flick that are not 100% size, minus the offset
+                    if(self.opts.isVert){
+                        startPoint = self.$slider.offset().top - parentTop;
+                    }else{
+                        startPoint = self.$slider.offset().left - parentLeft;
+                    }
+
+                    if(self.animating) return;
+                    
+                    // ATTENTION: in mobile device, in continous quick touchevents
+                    // touchstart won't fire, so set a flag forcely
+                    // enable touchstart callback do properly
+                    self.isMoving = true;
+
+                    self._clearTransition();
+                },
+                move: function(data) {
+                    if(self.animating || !self.isMoving) return;
+                    var deltaX = startPoint + data.delta.x,
+                        deltaY = startPoint + data.delta.y,
+                        transValue = '';
+
+                    if(self.opts.isVert){
+                        transValue = 'translate(0,' + deltaY + 'px) translateZ(0)';
+                    }else{
+                        transValue = 'translate(' + deltaX + 'px, 0) translateZ(0)';
+                    }
+                    self.$slider.css({
+                        '-webkit-transform': transValue,
+                        'transform': transValue
+                    });
+                },
+                end: function(data) {
+                    // ATTENTION: here is flag that determine if trigger the slider
+                    // one is a quick short swipe , another is distance diff
+                    // if(self.animating) return;
+                    self.isMoving = false;
+                    var deltaValue = self.opts.isVert ? data.delta.y : data.delta.x;
+                    if (data.deltatime < 250 && Math.abs(deltaValue) > 20 || Math.abs(deltaValue) > 100) {
+                        if (deltaValue > 0) {
+                            self.index--;
+                            self.index = self.index < 0 ? 0 : self.index;
+                        } else {
+                            self.index++;
+                            self.index = self.index > self.max - 1 ? self.max - 1 : self.index;
+                        }
+                    }
+                    self.jump(self.index);
+                }
+            });
+        },
+        _clearTransition: function(){
+            this.$slider.css({
+                '-webkit-transition-duration': '0s',
+                'transition-duration': '0s'
+            });
+        },
+        _setTransition: function(){
+            this.$slider.css({
+                '-webkit-transition-duration': '.4s',
+                'transition-duration': '.4s'
+            });
+        },
+        _destory: function(){
+
+        },
+        prev: function() {
+            var idx = this.index;
+            idx--;
+            if (!this.opts.autoSlide && idx < 0) return;
+            if (this.opts.autoSlide && idx < 0) {
+                idx = this.max - 1;
+            }
+            this.jump(idx);
+        },
+        next: function() {
+            var idx = this.index;
+            idx++;
+            if (!this.opts.autoSlide && idx > this.max - 1) return;
+            if (this.opts.autoSlide && idx > this.max - 1) {
+                idx = 0;
+            }
+            this.jump(idx);
+        },
+        jump: function(index){
+            var self = this;
+            this._setTransition();
+            this._jump(index, function(){
+                self.animating = true;
+                self.index = index;
+                self.opts.beforeSlide.call(self, self.index);
+            });
+            this._transitionCallback();
+        },
+
+        /**
+         * 静态位置变化
+         * @param  {[type]}   index    [description]
+         * @param  {Function} callback [description]
+         * @return {[type]}            [description]
+         */
+        _jump: function(index, callback) {
+            // get a width of px value, because % value does not work in andriod
+            var distance = this.opts.isVert ? this.$slider.height() : this.$slider.width(),
+                value = -distance * (index / this.max),
+                transValue = this.opts.isVert ? 'translate(0,' + value + 'px) translateZ(0)' : 'translate(' + value + 'px, 0) translateZ(0)';
+                
+            callback && callback();
+            this.$slider.css({
+                '-webkit-transform': transValue,
+                'transform': transValue
+            });
+        },
+        _transitionCallback: function(){
+            var self = this;
+            self.$slider.one(window.animationEvents.transitionEnd, function(){
+                self.animating = false;
+                self._clearTransition();
+                if(self.opts.isLoop){
+                    if(self.index === 0){
+                        self.index = self.max - self.clones;
+                    }
+                    if(self.index === self.max - 1){
+                        self.index = 1;
+                    }
+                    self._jump(self.index);
+                }
+                self.opts.afterSlide.call(self, self.index);
+            });
+        }
+    };
+
+    window.MuSlider = Slider;
+
+    $.fn.muSlider = function(options) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        this.each(function() {
+            var $this = $(this),
+                instance = $.fn.muSlider.instances[$this.data('muSlider')];
+
+            if (!instance) {
+                //cache the instance , use $.data in jquery, but in zepto data function is not fully supperted
+                $.fn.muSlider.instances[$.fn.muSlider.instances.i] = new Slider(this, options);
+                $this.data('muSlider', $.fn.muSlider.instances.i);
+                $.fn.muSlider.instances.i++;
+            }else if(typeof options === 'string' && instance[options]){
+                instance[options].apply(instance, args);
+            }
+
+        });
+        return this;
+    };
+
+    $.fn.muSlider.instances = {
+        i: 0
+    };
+
+})(window.Zepto || window.jQuery);
---
+++ js/mu/mu.touch.js
@@ -0,0 +1,139 @@
+/**
+ * -------------------------------------------------------------
+ * Copyright (c) 2014 All rights reserved.
+ * @version: 1.1.0
+ * @author: roeis
+ * @description: rewrite touch event, fix some issue in offical js
+ *               it can scroll vertically when trigger the touchstart of swipe
+ *               compatiable with pc mouse event
+ *               IMPORTANT: 
+ *               when u use swipeUp and swipeDown
+ *               prevent global touchmove event
+ * -------------------------------------------------------------
+ */
+(function($) {
+    'use strict';
+    var $elem,
+        start = {},
+        delta = {},
+        tapTimeout = null,
+        longTapTimeout = null,
+        isScrolling;
+
+    function cancelLongTap() {
+        if (longTapTimeout) clearTimeout(longTapTimeout);
+        longTapTimeout = null;
+    }
+
+    var events = {
+        start: function(event) {
+
+            var touches = event || event.originalEvent,
+                touch = touches.touches ? touches.touches[0] : event;
+
+            start = {
+                x: touch.clientX,
+                y: touch.clientY,
+                time: Date.now()
+            };
+            $elem = $(this._parentIfText(touch.target));
+
+            //used for testing the first touch event
+            isScrolling = undefined;
+
+            //reset delta
+            delta = {
+                x : 0,
+                y : 0
+            };
+
+            longTapTimeout = setTimeout(function(){
+                longTapTimeout = null;
+                $elem.trigger('longTap');
+            }, 700);
+        },
+
+        move: function(event) {
+            var touches = event || event.originalEvent,
+                touch = touches.touches ? touches.touches[0] : event;
+            if (touches && touches.length > 1 || event.scale && event.scale !== 1) return;
+
+            cancelLongTap();
+
+            delta = {
+                x: touch.clientX - start.x,
+                y: touch.clientY - start.y
+            };
+
+            //determine if scroll test has run !important!
+            //return trendence if updown or leftright
+            if (typeof isScrolling === 'undefined') {
+                isScrolling = isScrolling || Math.abs(delta.x) < Math.abs(delta.y);
+            }
+
+            if (!isScrolling) {
+                //issue: preventDefault to fire the touchmove and touchend event
+                event.preventDefault();
+            }
+        },
+
+        end: function(event) {
+
+            var duration = Date.now() - start.time,
+                isHorizontal = duration < 250 && Math.abs(delta.x) > 20 || Math.abs(delta.x) > 80,
+                isVertical = duration < 250 && Math.abs(delta.y) > 20 || Math.abs(delta.y) > 80,
+                isSwipeLeft = delta.x < 0,
+                isSwipeUp = delta.y < 0;
+
+            cancelLongTap();
+
+
+            if (!isScrolling && isHorizontal) {
+                $elem.trigger(isSwipeLeft ? 'swipeLeft' : 'swipeRight');
+            }else if(isScrolling && isVertical){
+                $elem.trigger(isSwipeUp ? 'swipeUp' : 'swipeDown');
+            }
+
+            tapTimeout = setTimeout(function(){
+                tapTimeout = null;
+                if(Math.abs(delta.x) < 5 && Math.abs(delta.y) < 5 && duration < 300) {
+                    var evt = $.Event('tap');
+
+                    // evt.cancelTouch = function(){
+                    //     if (tapTimeout) clearTimeout(tapTimeout);
+                    //     if (longTapTimeout) clearTimeout(longTapTimeout);
+                    // };
+                    
+                    $elem.trigger(evt);
+                }
+            }, 0);
+        },
+
+        _parentIfText: function(node) {
+            return 'tagName' in node ? node : node.parentNode;
+        }
+    };
+
+    var isMobile   = window.mu ? window.mu.detect.isMobile : true,
+        startEvent  = isMobile ? 'touchstart' : 'mousedown',
+        moveEvent   = isMobile ? 'touchmove' : 'mousemove',
+        endEvent    = isMobile ? 'touchend' : 'mouseup',
+        cancelEvent = isMobile ? 'touchcancel' : 'mouseup';
+
+    $(document).on(startEvent, function(event) {
+        events.start(event);
+    }).on(moveEvent, function(event) {
+        events.move(event);
+    }).on(endEvent, function(event) {
+        events.end(event);
+    }).on(cancelEvent, function() {
+        delta = {};
+    });
+
+    ['swipeLeft', 'swipeRight', 'swipeUp', 'swipeDown', 'tap', 'longTap'].forEach(function(eventName) {
+        $.fn[eventName] = function(callback) {
+            return this.on(eventName, callback);
+        };
+    });
+
+})(window.Zepto || window.jQuery);
